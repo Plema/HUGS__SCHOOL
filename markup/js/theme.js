52fc32d2358cec7e4416cfa7dd31203af9e1214a
SergeyVishnevskiy <serrzik13manager@gmail.com>
2021-12-06 12:02:28 +0200

    markup

---
+++ markup/js/theme.js
@@ -0,0 +1,8 @@
+var site_url = document.location.origin
+
+jQuery(document).ready(function ($) {
+  $('.menu-list li a').eq(0).removeClass('anchor')
+
+  //c	var menu = $(".menu-list").html();
+  //c	$('#test').text(menu);
+}) //endready
